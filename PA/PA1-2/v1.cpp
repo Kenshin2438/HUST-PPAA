60c38b112760408a03358428bfa322b2a2e36f09
Kenshin2438 <2953957899@qq.com>
2024-12-07 14:56:41 +0800

    pref: using semaphore & add waring info.

--- PA/PA1-2/v1.cpp
+++ PA/PA1-2/v1.cpp
@@ -1,28 +1,50 @@
 #include <format>
 #include <iostream>
-#include <mutex>
+#include <semaphore>
+#include <stop_token>
 #include <thread>
 #include <vector>
 
-auto main() -> int {
-  constexpr int N = 5;
+struct Chopstick : std::binary_semaphore {
+  explicit Chopstick() : std::binary_semaphore{1} {}
+};
 
-  std::vector<std::jthread> philosophers;
-  philosophers.reserve(N);
-  std::vector<std::mutex> chopstics(N);
+auto main(int /*argc*/, char** argv) -> int {
+  std::cerr << std::format(
+    "\e[91mThe implementation of {} is wrong.\n\e[0m"
+    "\e[93mPress [ENTER] to continue.\n\e[0m",
+    argv[0]);
+  std::cin.get();
 
-  auto event = [&chopstics](int i) -> void {
-    while (true) {
+  using namespace std::chrono_literals;
+
+  static constexpr int N = 5;
+
+  std::vector<Chopstick> chopstick(N);
+  auto event = [&chopstick](const std::stop_token& token, int i) {
+    int L = i;
+    int R = (i + 1) % N;
+
+    while (!token.stop_requested()) {
       std::cout << std::format("{} is now hungry.\n", i);
-      {
-        std::lock_guard<std::mutex> lock_left(chopstics[i]);
-        std::lock_guard<std::mutex> lock_right(chopstics[(i + 1) % N]);
-        std::cout << std::format("{} is now eating.\n", i);
-      }
+      chopstick[L].acquire();
+      chopstick[R].acquire();
+
+      std::cout << std::format("{} is now eating.\n", i);
+      // std::this_thread::sleep_for(200ms);
+
       std::cout << std::format("{} is now thinking.\n", i);
+      chopstick[L].release();
+      chopstick[R].release();
+
+      std::this_thread::yield();
     }
   };
 
+  std::vector<std::jthread> philosophers;
+  philosophers.reserve(N);
   for (int i = 0; i < N; i++) philosophers.emplace_back(event, i);
+  while (std::cin.get() != 'n') continue;
+
   return 0;
 }

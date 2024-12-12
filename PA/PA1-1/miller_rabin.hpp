df3e71cd45e758a54acbc15290390987d1818404
Kenshin2438 <2953957899@qq.com>
2024-12-13 00:48:36 +0800

    pref: changed SPRP bases for range(1, 1E8)

--- PA/PA1-1/miller_rabin.hpp
+++ PA/PA1-1/miller_rabin.hpp
@@ -1,40 +1,43 @@
 #pragma once
 
-#include <array>
 #include <bit>
+#include <cassert>
 #include <cstdint>
+#include <initializer_list>
 
-using u32 = uint32_t;
-using i32 = int32_t;
 using u64 = uint64_t;
 using i64 = int64_t;
 
-static inline constexpr auto modmul(const u64 a, const u64 b, const u64 M) -> u64 {
-  const i64 ret = static_cast<i64>(a * b - M * (u64)(1.L / M * a * b));
-  return ret + M * (ret < 0) - M * (ret >= (i64)M);
-}
+/** Miller–Rabin primality test: A probabilistic primality test
+REMARKS: https://miller-rabin.appspot.com/
+  Let a be primality witness. Let n be the number we test for primality.
+  1. Depending on your Miller-Rabin implementation, you may need to take a := a mod n;
+  2. When the witness a equals 0, the test should return that n is prime;
+  3. It is crucial to test all the bases and not just the bases less than n.
+SPRP: Deterministic Variant of Miller-Rabin primality test
+  Gerhard Jaeschke 1993 bases: [2, 7, 61] -> best solution: n <= 4'759'123'141
+*/
 
-static inline constexpr auto modpow(u64 a, u64 p, const u64 M) -> u64 {
-  u64 ret{1};
-  while (p) {
-    if (p & 1) ret = modmul(ret, a, M);
-    p >>= 1;
-    a = modmul(a, a, M);
-  }
-  return ret;
-}
-
-static inline auto miller_rabin_test(const u64 n) -> bool {
-  static constexpr std::array<u64, 7> SPRP{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+inline constexpr auto miller_rabin_test(const i64 n) noexcept -> bool {
+  assert(1 <= n && n <= 100'000'000LL);
   if (n == 1 || n % 6 % 4 != 1) return (n | 1) == 3;
 
-  const int t = std::countr_zero(n - 1);
-  const u64 k = std::rotr(n - 1, t);
-  for (const u64 &a : SPRP) {
-    u64 tmp = modpow(a, k, n);
+  constexpr auto modpow = [](i64 a, i64 n, i64 M) -> i64 {
+    i64 res = 1;
+    while (n) {
+      if (n & 1) res = res * a % M;
+      n >>= 1, a = a * a % M;
+    }
+    return res;
+  };
+
+  const int t = std::countr_zero(static_cast<u64>(n - 1));
+  const i64 k = (n - 1) >> t;
+  for (const i64 a : {2, 7, 61}) {
+    i64 tmp = modpow(a, k, n);
     if (tmp <= 1 || tmp == n - 1) continue;
     for (int i = 0; i < t; i++) {
-      tmp = modmul(tmp, tmp, n);
+      tmp = tmp * tmp % n;
       if (tmp == n - 1) break;
       if (i == t - 1) return false;
     }

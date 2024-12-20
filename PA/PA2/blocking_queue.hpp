7642bbfbd6d354a004021c60f44f338c25425ba5
Kenshin2438 <2953957899@qq.com>
2024-12-20 17:52:18 +0800

    refactor(blocking_queue): more readable.

--- PA/PA2/blocking_queue.hpp
+++ PA/PA2/blocking_queue.hpp
@@ -7,34 +7,35 @@
 
 namespace PA2 {
 
-template <typename T, std::size_t MAXN = 2048>
-  requires(std::is_trivial<T>::value && MAXN > 0 && (MAXN & (MAXN - 1)) == 0)
+template <typename T, std::size_t MAXN = 2048U>
+  requires(std::is_trivial<T>::value && MAXN >= 2 && (MAXN & (MAXN - 1)) == 0)
 class BLOCKING_QUEUE {
+ private:
+  static inline constexpr std::size_t MOD_MASK{MAXN - 1};
+
  public:
-  void add(const T& value) {
+  inline void add(const T& value) noexcept {
     std::lock_guard lock(mu_tail_);
-    enqueue.acquire();
-    buffer_[tail_] = value;
-    tail_ = (tail_ + 1) & (MAXN - 1);
-    dequeue.release();
+    const std::size_t index = (tail_ += 1U) & MOD_MASK;
+    sem_enqueue_.acquire();
+    buffer_[index] = value;
+    sem_dequeue_.release();
   }
-  void remove(T& value) {
+  inline void remove(T& value) noexcept {
     std::lock_guard lock(mu_head_);
-    dequeue.acquire();
-    value = std::move(buffer_[head_]);
-    head_ = (head_ + 1) & (MAXN - 1);
-    enqueue.release();
+    const std::size_t index = (head_ += 1U) & MOD_MASK;
+    sem_dequeue_.acquire();
+    value = buffer_[index];
+    sem_enqueue_.release();
   }
 
  private:
-  std::counting_semaphore<MAXN> enqueue{MAXN};
-  std::counting_semaphore<MAXN> dequeue{0};
+  std::counting_semaphore<MAXN> sem_enqueue_{MAXN};
+  std::counting_semaphore<MAXN> sem_dequeue_{0};
 
- private:
   std::mutex mu_head_;
   std::mutex mu_tail_;
 
- private:
   std::array<T, MAXN> buffer_{};
   std::size_t head_{};
   std::size_t tail_{};
